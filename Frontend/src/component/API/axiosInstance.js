a2dc95d07b590e02e28e9685538dd6f1e14e7fdd
varnikaBharalia <vbharalia11@gmail.com>
2026-05-30 13:39:25 +0530

    fixed frontend

--- Frontend/src/component/API/axiosInstance.js
+++ Frontend/src/component/API/axiosInstance.js
@@ -4,7 +4,8 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "https://blogaibackend.vercel.app/",
   // baseURL: "https://blogai-upqk.onrender.com/",
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL:"https://blogai-upqk.onrender.com/user/signin",
   withCredentials: true, 
 });
 
